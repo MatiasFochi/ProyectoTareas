ad677b9654b63dad131e193840ef7a65ef6a0e20
MatiasFochi <matiasfochi.dev@gmail.com>
2023-12-27 18:30:19 -0300

    Archivos iniciales del proyecto Tareas.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import ListaDeTareas from './componentes/ListaDeTareas';
+
+function App() {
+  return (
+    <div className='aplicacion-tareas'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo' />
+      </div>
+      <div className='tareas-lista-pricipal'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
