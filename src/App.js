eae8111573aca0a4ddaf2d79ab238f64dc3eae5d
MatiasFochi <matiasfochi.dev@gmail.com>
2023-12-27 18:41:27 -0300

    Correccion de error en App.js (Faltante de alt en img)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 import ListaDeTareas from './componentes/ListaDeTareas';
@@ -9,7 +9,8 @@ function App() {
       <div className='freecodecamp-logo-contenedor'>
         <img 
           src={freeCodeCampLogo}
-          className='freecodecamp-logo' />
+          className='freecodecamp-logo'
+          alt='freecodecamp logo' />
       </div>
       <div className='tareas-lista-pricipal'>
         <h1>Mis Tareas</h1>
