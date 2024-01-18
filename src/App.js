a34353c4f4c5b2d1235220d885b1f44232740d9a
MatiasFochi <matiasfochi.dev@gmail.com>
2024-01-18 12:15:08 -0300

    Actualizacion 18/01

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useState } from 'react';
 import ListaDeTareas from './componentes/ListaDeTareas';
 import MFClaro from './imagenes/LogoPersonalBlanco2.png';
-import MFOscuro from './imagenes/LogoPersonal2.png';
+import MFOscuro from './imagenes/LogoPersonal.png';
 
 function App() {
   
@@ -19,7 +19,7 @@ function App() {
           <img  className='logo' src = {MFClaro} alt = 'Logo oscuro Matias Fochi' style={{opacity: darkMode ? 1 : 0, transition: 'opacity 1s'}} />
           <img  className='logo' src = {MFOscuro} alt = 'Logo oscuro Matias Fochi' style={{opacity: darkMode ? 0 : 1, transition: 'opacity 1s'}} />
         </a>
-        <button className='btn btn-outline-custom' onClick={cambiarModo}>{darkMode? "Modo noche" : "Modo claro"}</button>
+        <button className={darkMode? 'btn btn-outline-custom modoOscuro' : 'btn btn-outline-custom'} onClick={cambiarModo}>{darkMode? "Modo noche" : "Modo claro"}</button>
       </div>
       <div className='tareas-lista-pricipal'>
         <h1 className={darkMode ? "titulo-tareas modoOscuro" : 'titulo-tareas'}>Mis Tareas</h1>
